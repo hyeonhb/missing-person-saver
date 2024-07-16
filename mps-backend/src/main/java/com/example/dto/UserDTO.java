dbc0b50aca70f51e96ee871ce0c6a377511a0b15
JIHEE JEONG <85853167+Insa14@users.noreply.github.com>
2024-07-17 00:08:51 +0900

    feat:BackEnd 기본 베이스
    BackEnd 기본 구조 생성 및 테스트 api 생성, DB 생성 및 연

---
+++ mps-backend/src/main/java/com/example/dto/UserDTO.java
@@ -0,0 +1,31 @@
+package com.example.dto;
+
+public class UserDTO {
+    private Long id;
+    private String name;
+    private String telno;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTelno() {
+        return telno;
+    }
+
+    public void setTelno(String telno) {
+        this.telno = telno;
+    }
+}
