8ab2b55757fa9199ef4f3a49b1c07721978c114b
JIHEE JEONG <85853167+Insa14@users.noreply.github.com>
2024-07-21 14:50:21 +0900

    FEAT.데이터 베이스 스키마 추가

---
+++ mps-backend/src/main/java/com/example/entity/MissingPerson.java
@@ -0,0 +1,59 @@
+package com.example.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@AllArgsConstructor
+
+@Table(name = "MISSING_PERSON")
+public class MissingPerson {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MP_ID")
+    private Integer mpId;
+
+    @Column(name = "NAME", nullable = false, length = 10)
+    private String name;
+
+    @Column(name = "AGE", nullable = false)
+    private Integer age;
+
+    @Column(name = "SEXDSTN_DSCD", nullable = false)
+    private Integer sexdstnDscd;
+
+    @Column(name = "REALISE_DATE", nullable = false, length = 8)
+    private String realiseDate;
+
+    @Column(name = "REALISE_PLACE", length = 30)
+    private String realisePlace;
+
+    @Column(name = "HEIGHT")
+    private Integer height;
+
+    @Column(name = "WEIGHT")
+    private Integer weight;
+
+    @Column(name = "FRM_DSCD", length = 10)
+    private String frmDscd;
+
+    @Column(name = "FACE_SHAPE_DSCD", length = 10)
+    private String faceShapeDscd;
+
+    @Column(name = "HAIR_COLOR_DSCD", length = 10)
+    private String hairColorDscd;
+
+    @Column(name = "HAIR_SHAPE_DSCD", length = 10)
+    private String hairShapeDscd;
+
+    @Column(name = "DRESSING_DSCD", length = 10)
+    private String dressingDscd;
+}
