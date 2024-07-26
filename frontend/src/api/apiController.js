af93325479ba02512361ab7303357102357472b8
hyeonhb <hyeon.hwangbo@mytrum.com>
2024-07-26 19:21:47 +0900

    refactor: try/catch문과 API fetch() 하는 부분을 한단계 더 추상화

--- frontend/src/api/apiController.js
+++ frontend/src/api/apiController.js
@@ -2,29 +2,27 @@ import { isEmptyObject } from "../utils/validator";
 
 const apiController = {
     get: async (url, query = {}) => {
-        try {
-            const queryString = getQueryString(query)
+        const queryString = getQueryString(query)
 
-            const response = await fetch(url + queryString);
-            return await response.json();
-        } catch(err) {
-            console.error(`${url}에서 에러 발생`, err);
-            return err;
-        }
+        return await fetchApi(url + queryString);
     },
     post: async (url, param = {}, option = {}) => {
-        try {
-            const response = await fetch(url, {
-                body: param,
-                ...option,
-                method: 'POST',
-            });
-            return await response.json();
-        } catch(err) {
-            console.error(`${url}에서 에러 발생`, err);
-            return err;
-        }
+        return await fetchApi(url, {
+            body: param,
+            ...option,
+            method: 'POST',
+        });
     },
+}
+
+async function fetchApi(url, option) {
+    try {
+        const response = await fetch(url, option);
+        return await response.json();
+    } catch(err) {
+        console.error(`${url}에서 에러 발생`, err);
+        return err;
+    }
 }
 
 function getQueryString(query) {
