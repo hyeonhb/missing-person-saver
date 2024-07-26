0b70d4220874622e7093f6a3aa9e8850e854730a
hyeonhb <hyeon.hwangbo@mytrum.com>
2024-07-26 17:51:21 +0900

    feat: 에러 공통 호출문에 try/catch문 추가

--- frontend/src/api/apiController.js
+++ frontend/src/api/apiController.js
@@ -1,20 +1,29 @@
-
 import { isEmptyObject } from "../utils/validator";
 
 const apiController = {
     get: async (url, query = {}) => {
-        const queryString = getQueryString(query)
+        try {
+            const queryString = getQueryString(query)
 
-        const response = await fetch(url + queryString);
-        return await response.json();
+            const response = await fetch(url + queryString);
+            return await response.json();
+        } catch(err) {
+            console.error(`${url}에서 에러 발생`, err);
+            return err;
+        }
     },
     post: async (url, param = {}, option = {}) => {
-        const response = await fetch(url, {
-            body: param,
-            ...option,
-            method: 'POST',
-        });
-        return await response.json();
+        try {
+            const response = await fetch(url, {
+                body: param,
+                ...option,
+                method: 'POST',
+            });
+            return await response.json();
+        } catch(err) {
+            console.error(`${url}에서 에러 발생`, err);
+            return err;
+        }
     },
 }
 
