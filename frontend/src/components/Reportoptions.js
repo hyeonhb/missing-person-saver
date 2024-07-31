3c2d9ebae16ce654a8ca38f31b5abb4ce84b7e10
hyeonhb <hyeonnhb@gmail.com>
2024-08-01 04:19:29 +0900

    style: Report 방식 선택창 디자인 작업 완료

--- frontend/src/components/Reportoptions.js
+++ frontend/src/components/Reportoptions.js
@@ -22,14 +22,17 @@ const ReportOptions = ({ onClose }) => {
   return (
     <div className="report-options-modal">
       <div className="report-options-content">
-        <h2>▼제보 방법을 선택해주세요▼</h2>
-        <div className="report-options-grid">
-          <div className="report-option" onClick={() => handleOptionClick('message')}>메세지<br />제보하기</div>
-          <div className="report-option" onClick={() => handleOptionClick('call')}>담당 경찰관<br />전화연결</div>
-          <div className="report-option" onClick={() => handleOptionClick('gps')}>현재 위치<br />제보하기</div>
-          <div className="report-option" onClick={() => handleOptionClick('image')}>사진<br />제보하기</div>
+        <div className="report-header">
+          <h2>제보 방법을 선택해주세요</h2>
+          <p className='report-closer' onClick={onClose}>✕</p>
         </div>
-        <button className="close-button" onClick={onClose}>닫기</button>
+
+        <div className="report-options-grid">
+          <div className="report-option" onClick={() => handleOptionClick('message')}>메세지 제보하기</div>
+          <div className="report-option" onClick={() => handleOptionClick('call')}>담당 경찰관 전화연결</div>
+          <div className="report-option" onClick={() => handleOptionClick('gps')}>현재 위치 제보하기</div>
+          <div className="report-option" onClick={() => handleOptionClick('image')}>사진 제보하기</div>
+        </div>
       </div>
       {selectedOption === 'message' && <ReportMessage onClose={closeModal} onSubmit={(message) => alert(`제보 메시지: ${message}`)} />}
       {selectedOption === 'call' && <ReportCall onClose={closeModal} />}
