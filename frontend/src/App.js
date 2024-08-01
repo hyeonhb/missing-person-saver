ea0646af11747dfde128756a655a8108059f768a
dwomp <logos050304@gmail.com>
2024-08-02 03:29:45 +0900

    feat: 메인 도입부 화면 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
-import { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 // Styles
 import './styles/App.css';
+import './styles/InitialScreen.css';
+
 // Components
 import Header from './components/Header.js';
+import InitialScreen from './pages/InitialScreen.js';
 import Chat from './pages/Chat.js';
 // Utils
 import storage from './utils/storage.js';
 
 function App() {
+  const [showInitialScreen, setShowInitialScreen] = useState(true);
+
   useEffect(() => {
     setMisperKey();
   }, []);
@@ -15,15 +20,22 @@ function App() {
   function setMisperKey() {
     const urlParams = new URLSearchParams(window.location.search);
     const key = urlParams.get('misper') || 'TEST_KEY';
-
     storage.set.misperKey(key);
   }
 
+  const handleAnimationComplete = () => {
+    setShowInitialScreen(false);
+  };
+
   return (
     <section className='container'>
-      <Header title='MPS(실종자 제보 서비스)' />
-      <Chat />
-    </section>   
+      {!showInitialScreen && <Header title='MPS(실종자 제보 서비스)' />}
+      {showInitialScreen ? (
+        <InitialScreen onAnimationComplete={handleAnimationComplete} />
+      ) : (
+        <Chat />
+      )}
+    </section>
   );
 }
 
