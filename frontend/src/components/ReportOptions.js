00a404ef414e91ababe6510fdd22a9b7d1100682
hyeonhb <hyeonnhb@gmail.com>
2024-08-01 16:01:26 +0900

    feat: 하위 옵션 선택시 제보 방식 선택 modal은 숨기기

--- frontend/src/components/ReportOptions.js
+++ frontend/src/components/ReportOptions.js
@@ -9,31 +9,34 @@ import ReportGPS from './reports/ReportGPS';
 import ReportImage from './reports/ReportImage';
 
 const ReportOptions = ({ onClose }) => {
-  const [selectedOption, setSelectedOption] = useState(null);
+  const [selectedOption, setSelectedOption] = useState('default');
 
   const handleOptionClick = (option) => {
     setSelectedOption(option);
   };
 
   const closeModal = () => {
-    setSelectedOption(null);
+    setSelectedOption('default');
   };
 
   return (
     <div className="report-options-modal">
-      <div className="report-options-content">
-        <div className="report-header">
-          <h2>제보 방법을 선택해주세요</h2>
-          <p className='report-closer' onClick={onClose}>✕</p>
-        </div>
+        {selectedOption === 'default' && 
+          <div className="report-options-content">
+          <div className="report-header">
+            <h2>제보 방법을 선택해주세요</h2>
+            <p className='report-closer' onClick={onClose}>✕</p>
+          </div>
 
-        <div className="report-options-grid">
-          <div className="report-option" onClick={() => handleOptionClick('message')}>메세지 제보하기</div>
-          <div className="report-option" onClick={() => handleOptionClick('call')}>담당 경찰관 전화연결</div>
-          <div className="report-option" onClick={() => handleOptionClick('gps')}>현재 위치 제보하기</div>
-          <div className="report-option" onClick={() => handleOptionClick('image')}>사진 제보하기</div>
+          <div className="report-options-grid">
+            <div className="report-option" onClick={() => handleOptionClick('message')}>메세지 제보하기</div>
+            <div className="report-option" onClick={() => handleOptionClick('call')}>담당 경찰관 전화연결</div>
+            <div className="report-option" onClick={() => handleOptionClick('gps')}>현재 위치 제보하기</div>
+            <div className="report-option" onClick={() => handleOptionClick('image')}>사진 제보하기</div>
+          </div>
         </div>
-      </div>
+        }
+
       {selectedOption === 'message' && <ReportMessage onClose={closeModal} onSubmit={(message) => alert(`제보 메시지: ${message}`)} />}
       {selectedOption === 'call' && <ReportCall onClose={closeModal} />}
       {selectedOption === 'gps' && <ReportGPS onClose={closeModal} />}
